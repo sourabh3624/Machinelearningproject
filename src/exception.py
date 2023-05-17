745bf4db4006573993f9732805a4b24124f7bef5
sourabh bainsla <georbainsla.36georgian@gmail.com>
2023-05-17 05:53:57 +0530

    data ingestion is done

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging 
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -17,12 +17,13 @@ class CustomException(Exception):
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
     def __str__(self):
         return  self.error_message
-    
+'''   
 if __name__=="__main__":
 
     try:
         a=1/0
     except Exception as e:
-        logging.info("divide by zero")
+        logging.info("devide by zero")
         raise CustomException(e,sys)
         
+'''
